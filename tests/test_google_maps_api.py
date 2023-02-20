64937f87017df5256581499933828ddf1659f27a
DavydovQAA <ghostinside80@gmail.com>
2023-02-21 00:30:51 +0300

    google maps api commit

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -9,5 +9,22 @@ class Test_create_place():
     def test_create_new_place(self):
 
         print('Метод POST')
-        result_post: Response = Google_maps_api.create_new_place()
+        result_post = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get('place_id')
+
+        print('Метод GET POST')
+        result_get = Google_maps_api.get_new_place(place_id)
+
+        print('Метод PUT')
+        result_put = Google_maps_api.put_new_place(place_id)
+
+        print('Метод GET PUT')
+        result_get = Google_maps_api.get_new_place(place_id)
+
+        print('Метод DELETE')
+        result_delete = Google_maps_api.delete_new_place(place_id)
+
+        print('Метод GET POST DELETE')
+        result_get = Google_maps_api.get_new_place(place_id)
 
--- utils/api.py
+++ utils/api.py
@@ -1,34 +1,30 @@
 from utils.http_methods import Http_method
 
-
 """Методы для тестирования Google maps api"""
 
-base_url = 'https://rahulshettyacademy.com'       # Базовая URL
-key = '?key=qaclick123'                            # Параметр для всех запросов:
+base_url = 'https://rahulshettyacademy.com'  # Базовая URL
+key = '?key=qaclick123'  # Параметр для всех запросов:
 
 
 class Google_maps_api():
 
-
     """Метод для создания новой локации"""
-
     @staticmethod
     def create_new_place():
-
         json_for_create_new_place = {
-            "location":{
-            "lat":-38.383494,
-            "lng":33.427362
-            },"accuracy":50,
-            "name":"Frontline house",
-            "phone_number":"(+91) 983 893 3937",
-            "address":"29, side layout, cohen 09",
-            "types":[
-            "shoe park",
-            "shop"
+            "location": {
+                "lat": -38.383494,
+                "lng": 33.427362
+            }, "accuracy": 50,
+            "name": "Frontline house",
+            "phone_number": "(+91) 983 893 3937",
+            "address": "29, side layout, cohen 09",
+            "types": [
+                "shoe park",
+                "shop"
             ],
-            "website":"http://google.com",
-            "language":"French-IN"
+            "website": "http://google.com",
+            "language": "French-IN"
         }
 
         post_resource = '/maps/api/place/add/json'  # Ресурс метода Post
@@ -38,5 +34,46 @@ class Google_maps_api():
         print(reslut_post.text)
         return reslut_post
 
+    """Метод для проверки новой локации"""
+    @staticmethod
+    def get_new_place(place_id):
+
+        get_resource = '/maps/api/place/get/json'        # Ресурс метода Get
+        get_url = base_url + get_resource + key + '&place_id=' + place_id
+        print(get_url)
+        result_get = Http_method.get(get_url)
+        print(result_get.text)
+        return result_get
+
+    """Метод для изменения новой локации"""
+    @staticmethod
+    def put_new_place(place_id):
+
+        put_resource = '/maps/api/place/update/json'    # Ресурс метода PUT
+        put_url = base_url + put_resource + key
+        print(put_url)
+        json_for_update_new_location = {
+            "place_id": place_id,
+            "address":"100 Lenina street, RU",
+            "key":"qaclick123"
+         }
+
+        result_put = Http_method.put(put_url, json_for_update_new_location)
+        print(result_put.text)
+        return result_put
 
 
+    """Метод для удаления новой локации"""
+    @staticmethod
+    def delete_new_place(place_id):
+
+        delete_resource = '/maps/api/place/delete/json'    # Ресурс метода DELETE
+        put_url = base_url + delete_resource + key
+        print(put_url)
+        json_for_delete_new_location = {
+            "place_id": place_id
+         }
+
+        result_delete = Http_method.delete(put_url, json_for_delete_new_location)
+        print(result_delete.text)
+        return result_delete
