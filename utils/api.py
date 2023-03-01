594eac7ce58f5098f2711da375fe9a688d769ad1
DavydovQAA <ghostinside80@gmail.com>
2023-03-02 00:59:29 +0300

    Final commit

--- utils/api.py
+++ utils/api.py
@@ -2,15 +2,16 @@ from utils.http_methods import Http_method
 
 """Методы для тестирования Google maps api"""
 
-base_url = 'https://rahulshettyacademy.com'  # Базовая URL
-key = '?key=qaclick123'  # Параметр для всех запросов:
-
+base_url = "https://rahulshettyacademy.com"         # Базовая URL
+key = "?key=qaclick123"          #Параметр для всех запросов
 
 class Google_maps_api():
 
     """Метод для создания новой локации"""
+
     @staticmethod
     def create_new_place():
+
         json_for_create_new_place = {
             "location": {
                 "lat": -38.383494,
@@ -25,55 +26,58 @@ class Google_maps_api():
             ],
             "website": "http://google.com",
             "language": "French-IN"
+
         }
 
-        post_resource = '/maps/api/place/add/json'  # Ресурс метода Post
+        post_resource = "/maps/api/place/add/json"      # Ресурс метода Post
         post_url = base_url + post_resource + key
         print(post_url)
-        reslut_post = Http_method.post(post_url, json_for_create_new_place)
-        print(reslut_post.text)
-        return reslut_post
+        result_post = Http_method.post(post_url, json_for_create_new_place)
+        print(result_post.text)
+        return result_post
+
 
     """Метод для проверки новой локации"""
+
     @staticmethod
     def get_new_place(place_id):
 
-        get_resource = '/maps/api/place/get/json'        # Ресурс метода Get
-        get_url = base_url + get_resource + key + '&place_id=' + place_id
+        get_resource = "/maps/api/place/get/json"       # Ресурс метода Get
+        get_url = base_url + get_resource + key + "&place_id=" + place_id
         print(get_url)
         result_get = Http_method.get(get_url)
         print(result_get.text)
         return result_get
 
-    """Метод для изменения новой локации"""
+    """Метод для зменения новой локации"""
+
     @staticmethod
     def put_new_place(place_id):
 
-        put_resource = '/maps/api/place/update/json'    # Ресурс метода PUT
+        put_resource = "/maps/api/place/update/json"       # Ресурс метода Put
         put_url = base_url + put_resource + key
         print(put_url)
         json_for_update_new_location = {
             "place_id": place_id,
-            "address":"100 Lenina street, RU",
-            "key":"qaclick123"
-         }
-
+            "address": "100 Lenina street, RU",
+            "key": "qaclick123"
+        }
         result_put = Http_method.put(put_url, json_for_update_new_location)
         print(result_put.text)
         return result_put
 
 
     """Метод для удаления новой локации"""
+
     @staticmethod
     def delete_new_place(place_id):
 
-        delete_resource = '/maps/api/place/delete/json'    # Ресурс метода DELETE
+        delete_resource = "/maps/api/place/delete/json"       # Ресурс метода Delete
         put_url = base_url + delete_resource + key
         print(put_url)
         json_for_delete_new_location = {
             "place_id": place_id
-         }
-
+        }
         result_delete = Http_method.delete(put_url, json_for_delete_new_location)
         print(result_delete.text)
         return result_delete
--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,28 +1,46 @@
+import allure
 import requests
 
-"""Список HTTP методов"""
+from utils.logger import Logger
 
+"""Список HTTP методов"""
 
 class Http_method:
     headers = {'Content-Type': 'application/json'}
     cookie = ""
 
+
     @staticmethod
     def get(url):
-        result = requests.get(url, headers=Http_method.headers, cookies=Http_method.cookie)
-        return result
+        with allure.step("GET"):
+            Logger.add_request(url, method="GET")
+            result = requests.get(url, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
+
 
     @staticmethod
     def post(url, body):
-        result = requests.post(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
-        return result
+        with allure.step("POST"):
+            Logger.add_request(url, method="POST")
+            result = requests.post(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
+
 
     @staticmethod
     def put(url, body):
-        result = requests.put(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
-        return result
+        with allure.step("PUT"):
+            Logger.add_request(url, method="PUT")
+            result = requests.put(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
+
 
     @staticmethod
     def delete(url, body):
-        result = requests.delete(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
-        return result
+        with allure.step("DELETE"):
+            Logger.add_request(url, method="DELETE")
+            result = requests.delete(url, json=body, headers=Http_method.headers, cookies=Http_method.cookie)
+            Logger.add_response(result)
+            return result
